78443de073bedd9e391d70a762dd72e3f66e347c
Wesley Ellis <tahnok@gmail.com>
2016-03-19 19:31:34 -0400

    sorta working more

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,19 @@ extern crate regex;
 use regex::Regex;
 use std::str;
 use std::process::Command;
+use std::process::Output;
 
 fn main() {
     println!("Hello, world!");
 
-    let new_cursor = connections("");
+    let re = Regex::new("User \"(.+)\" registered").unwrap();
+    let new_cursor = connections("", &re);
+    println!("{}", new_cursor);
+    let new_cursor = connections(&new_cursor, &re);
     println!("{}", new_cursor);
 }
 
-fn connections(cursor: &str) -> String {
-    let re = Regex::new("User \"(.+)\" registered").unwrap();
+fn connections(cursor: &str, re: &Regex) -> String {
     let mut command = Command::new("journalctl");
 
     command.arg("_COMM=ngircd")
@@ -22,19 +25,22 @@ fn connections(cursor: &str) -> String {
            .arg("--show-cursor");
 
     if !cursor.is_empty() {
-        command.arg("--after-cursor=")
-               .arg(cursor);
+        command.arg(format!("--after-cursor='{}'", cursor));
     }
 
-    let output = command.output().unwrap();
+    println!("command {:?}", command);
+    let Output {status, stdout, stderr} = command.output().unwrap();
 
-    println!("status: {}", output.status);
-    println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+    if !status.success() {
+        println!("status: {}", status);
+        println!("stderr: {}", str::from_utf8(&stderr).unwrap());
+        panic!();
+    }
 
-    let logs = str::from_utf8(&output.stdout).unwrap();
+    let logs = str::from_utf8(&stdout).unwrap();
     for user in re.captures_iter(logs) {
         println!("{}", user.at(1).unwrap_or("??"));
     }
 
-    String::from(logs.lines().last().unwrap()).clone()
+    String::from(&logs.lines().last().unwrap()[11..]).clone()
 }
