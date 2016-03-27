cb228f1c2dfeb01648fbab1bb0ed4f38fcdc88b4
Wesley Ellis <tahnok@gmail.com>
2016-03-27 19:39:17 -0400

    message when a user connects

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,44 @@
+extern crate irc;
 #[macro_use]
 extern crate lazy_static;
 extern crate regex;
 
+use irc::client::prelude::*;
 use regex::Regex;
 use std::str;
 use std::collections::HashSet;
-use std::process::Command;
-use std::process::Output;
+use std::process;
+use std::thread::sleep;
+use std::time::Duration;
 
 lazy_static! {
     static ref USER: Regex = Regex::new(r#"User "(.+)!(.+)" registered"#).unwrap();
 }
 
 fn main() {
-    let (new_cursor, users) = connections("");
-    for user in users.iter() {
-        println!("{}", user);
+    let config = Config {
+        nickname: Some(format!("bott")),
+        server: Some(format!("127.0.0.1")),
+        channels: Some(vec![format!("#main")]),
+        .. Default::default()
+    };
+    let server = IrcServer::from_config(config).unwrap();
+    server.identify().unwrap();
+
+    let mut cursor = String::new();
+    loop {
+        let (new_cursor, nicks) = get_nicks(&cursor);
+        cursor = new_cursor;
+        for user in nicks.iter() {
+            server.send_privmsg("wes", format!("{} has joined", user).as_str());
+            println!("{}", user);
+        }
+        sleep(Duration::new(5,0));
     }
-    connections(&new_cursor);
 }
 
-fn connections(cursor: &str) -> (String, HashSet<String>) {
-    let mut command = Command::new("journalctl");
+fn get_nicks(cursor: &String) -> (String, HashSet<String>) {
+    let mut command = process::Command::new("journalctl");
 
     command.arg("_COMM=ngircd")
            .arg("-o")
@@ -33,7 +50,7 @@ fn connections(cursor: &str) -> (String, HashSet<String>) {
         command.arg(format!("--after-cursor={}", cursor));
     }
 
-    let Output {status, stdout, stderr} = command.output().unwrap();
+    let process::Output {status, stdout, stderr} = command.output().unwrap();
 
     if !status.success() {
         println!("status: {}", status);
